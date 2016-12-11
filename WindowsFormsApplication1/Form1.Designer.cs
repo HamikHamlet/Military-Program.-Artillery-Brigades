948f02aac836d238e1873985093de79add22411a
HamikHamlet <phamik@mail.ru>
2016-12-11 23:16:07 +0400

    New Form UI Design

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -50,6 +50,8 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textBoxAltilerTitle = new System.Windows.Forms.TextBox();
             this.numericUpDownaltiler = new System.Windows.Forms.NumericUpDown();
             this.textBoxArtilleryModel = new System.Windows.Forms.TextBox();
@@ -63,21 +65,21 @@
             this.buttonSelectDB = new System.Windows.Forms.Button();
             this.buttonSaveExcel = new System.Windows.Forms.Button();
             this.buttonSaveDB = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.groupBoxArmy.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).BeginInit();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownaltiler)).BeginInit();
             this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.DarkRed;
             this.label1.Location = new System.Drawing.Point(8, 65);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(65, 20);
@@ -106,7 +108,7 @@
             this.groupBoxArmy.Controls.Add(this.label3);
             this.groupBoxArmy.Controls.Add(this.label2);
             this.groupBoxArmy.Controls.Add(this.label1);
-            this.groupBoxArmy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBoxArmy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.groupBoxArmy.Location = new System.Drawing.Point(12, 4);
             this.groupBoxArmy.Name = "groupBoxArmy";
             this.groupBoxArmy.Size = new System.Drawing.Size(319, 434);
@@ -125,7 +127,9 @@
             // label14
             // 
             this.label14.AutoSize = true;
+            this.label14.BackColor = System.Drawing.Color.Transparent;
             this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label14.ForeColor = System.Drawing.Color.DarkRed;
             this.label14.Location = new System.Drawing.Point(8, 29);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(125, 20);
@@ -180,7 +184,9 @@
             // label13
             // 
             this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Transparent;
             this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.ForeColor = System.Drawing.Color.DarkRed;
             this.label13.Location = new System.Drawing.Point(6, 393);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(63, 20);
@@ -354,16 +360,18 @@
             // 
             // numericUpDownAge
             // 
-            this.numericUpDownAge.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.numericUpDownAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.numericUpDownAge.Location = new System.Drawing.Point(145, 192);
+            this.numericUpDownAge.BackColor = System.Drawing.Color.Green;
+            this.numericUpDownAge.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.numericUpDownAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownAge.ForeColor = System.Drawing.SystemColors.Info;
+            this.numericUpDownAge.Location = new System.Drawing.Point(240, 192);
             this.numericUpDownAge.Minimum = new decimal(new int[] {
             18,
             0,
             0,
             0});
             this.numericUpDownAge.Name = "numericUpDownAge";
-            this.numericUpDownAge.Size = new System.Drawing.Size(90, 26);
+            this.numericUpDownAge.Size = new System.Drawing.Size(73, 27);
             this.numericUpDownAge.TabIndex = 11;
             this.numericUpDownAge.Value = new decimal(new int[] {
             18,
@@ -398,7 +406,9 @@
             // label8
             // 
             this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.ForeColor = System.Drawing.Color.DarkRed;
             this.label8.Location = new System.Drawing.Point(6, 354);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(116, 20);
@@ -408,7 +418,9 @@
             // label7
             // 
             this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.DarkRed;
             this.label7.Location = new System.Drawing.Point(6, 317);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(60, 20);
@@ -418,7 +430,9 @@
             // label6
             // 
             this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.DarkRed;
             this.label6.Location = new System.Drawing.Point(6, 275);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(71, 20);
@@ -428,7 +442,9 @@
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.DarkRed;
             this.label5.Location = new System.Drawing.Point(6, 235);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(69, 20);
@@ -438,7 +454,9 @@
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.DarkRed;
             this.label4.Location = new System.Drawing.Point(6, 192);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(66, 20);
@@ -448,7 +466,9 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.DarkRed;
             this.label3.Location = new System.Drawing.Point(6, 149);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(108, 20);
@@ -458,7 +478,9 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.DarkRed;
             this.label2.Location = new System.Drawing.Point(6, 107);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(102, 20);
@@ -477,13 +499,30 @@
             this.groupBox1.Controls.Add(this.label11);
             this.groupBox1.Controls.Add(this.label10);
             this.groupBox1.Controls.Add(this.label9);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.groupBox1.Location = new System.Drawing.Point(337, 4);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(409, 212);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Հրետանու տվյալներ";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(294, 111);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(109, 73);
+            this.pictureBox2.TabIndex = 19;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(294, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(109, 73);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
             // 
             // textBoxAltilerTitle
             // 
@@ -495,9 +534,11 @@
             // 
             // numericUpDownaltiler
             // 
-            this.numericUpDownaltiler.BackColor = System.Drawing.Color.White;
-            this.numericUpDownaltiler.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.numericUpDownaltiler.Location = new System.Drawing.Point(198, 159);
+            this.numericUpDownaltiler.BackColor = System.Drawing.Color.Green;
+            this.numericUpDownaltiler.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.numericUpDownaltiler.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownaltiler.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.numericUpDownaltiler.Location = new System.Drawing.Point(215, 161);
             this.numericUpDownaltiler.Maximum = new decimal(new int[] {
             2016,
             0,
@@ -509,7 +550,7 @@
             0,
             0});
             this.numericUpDownaltiler.Name = "numericUpDownaltiler";
-            this.numericUpDownaltiler.Size = new System.Drawing.Size(90, 26);
+            this.numericUpDownaltiler.Size = new System.Drawing.Size(73, 25);
             this.numericUpDownaltiler.TabIndex = 16;
             this.numericUpDownaltiler.Value = new decimal(new int[] {
             1950,
@@ -537,6 +578,7 @@
             // 
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.ForeColor = System.Drawing.Color.DarkRed;
             this.label12.Location = new System.Drawing.Point(6, 119);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(66, 20);
@@ -547,6 +589,7 @@
             // 
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.ForeColor = System.Drawing.Color.DarkRed;
             this.label11.Location = new System.Drawing.Point(6, 161);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(210, 20);
@@ -557,6 +600,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.ForeColor = System.Drawing.Color.DarkRed;
             this.label10.Location = new System.Drawing.Point(6, 77);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(59, 20);
@@ -567,6 +611,7 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.ForeColor = System.Drawing.Color.DarkRed;
             this.label9.Location = new System.Drawing.Point(6, 35);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(65, 20);
@@ -579,7 +624,7 @@
             this.groupBox2.Controls.Add(this.buttonSelectDB);
             this.groupBox2.Controls.Add(this.buttonSaveExcel);
             this.groupBox2.Controls.Add(this.buttonSaveDB);
-            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.groupBox2.Location = new System.Drawing.Point(337, 222);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(409, 216);
@@ -589,7 +634,7 @@
             // 
             // buttonSaveFile
             // 
-            this.buttonSaveFile.BackColor = System.Drawing.Color.White;
+            this.buttonSaveFile.BackColor = System.Drawing.Color.SaddleBrown;
             this.buttonSaveFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonSaveFile.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.buttonSaveFile.ForeColor = System.Drawing.Color.Black;
@@ -602,7 +647,7 @@
             // 
             // buttonSelectDB
             // 
-            this.buttonSelectDB.BackColor = System.Drawing.Color.White;
+            this.buttonSelectDB.BackColor = System.Drawing.Color.ForestGreen;
             this.buttonSelectDB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonSelectDB.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.buttonSelectDB.ForeColor = System.Drawing.Color.Black;
@@ -615,7 +660,7 @@
             // 
             // buttonSaveExcel
             // 
-            this.buttonSaveExcel.BackColor = System.Drawing.Color.White;
+            this.buttonSaveExcel.BackColor = System.Drawing.Color.ForestGreen;
             this.buttonSaveExcel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonSaveExcel.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.buttonSaveExcel.ForeColor = System.Drawing.Color.Black;
@@ -629,7 +674,7 @@
             // 
             // buttonSaveDB
             // 
-            this.buttonSaveDB.BackColor = System.Drawing.Color.White;
+            this.buttonSaveDB.BackColor = System.Drawing.Color.SaddleBrown;
             this.buttonSaveDB.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonSaveDB.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.buttonSaveDB.ForeColor = System.Drawing.Color.Black;
@@ -640,23 +685,6 @@
             this.buttonSaveDB.Text = "Գրանցում Բազայում";
             this.buttonSaveDB.UseVisualStyleBackColor = false;
             this.buttonSaveDB.Click += new System.EventHandler(this.buttonSaveDB_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(294, 32);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(109, 73);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 18;
-            this.pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(294, 111);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(109, 73);
-            this.pictureBox2.TabIndex = 19;
-            this.pictureBox2.TabStop = false;
             // 
             // Form1
             // 
@@ -674,10 +702,10 @@
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAge)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownaltiler)).EndInit();
             this.groupBox2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
