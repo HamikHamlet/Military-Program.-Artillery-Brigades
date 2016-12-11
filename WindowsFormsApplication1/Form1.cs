7e6b44362624727ead39b1a4540d5e8b252feed1
HamikHamlet <phamik@mail.ru>
2016-12-11 18:43:00 +0400

    Add WriteDataToBD class  and new functions

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -14,13 +14,13 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-        private string connectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\User\Source\Repos\Console\Military-Program.-Artillery-Brigades4\WindowsFormsApplication1\bin\Debug\armydata.mdf;Integrated Security=True";
         public Form1()
         {
             InitializeComponent();
             Selected();
+           
         }
-
+        DataModel datamodel;
         private async void buttonSaveExcel_Click(object sender, EventArgs e)
         {
             WriteDataToExcel writedatatoexcel;
@@ -28,7 +28,7 @@ namespace WindowsFormsApplication1
             if (chechTextbox != -1)
             {
                 string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\GeneratedFile";
-                DataModel datamodel = new DataModel(textBoxPassportID.Text, textboxFirstName.Text, textBoxLastName.Text, textBoxFName.Text, Convert.ToInt16(numericUpDownAge.Value), comboBoxTitle.Text, comboBoxClassical.Text, comboBoxCompany.Text, comboBoxBattalion.Text, comboBoxBowl.Text, textBoxArtilleryName.Text, textBoxArtilleryModel.Text, textBoxAltilerTitle.Text, Convert.ToInt16(numericUpDownaltiler.Value));
+                 datamodel = new DataModel(textBoxPassportID.Text, textboxFirstName.Text, textBoxLastName.Text, textBoxFName.Text, Convert.ToInt16(numericUpDownAge.Value), comboBoxTitle.Text, comboBoxClassical.Text, comboBoxCompany.Text, comboBoxBattalion.Text, comboBoxBowl.Text, textBoxArtilleryName.Text, textBoxArtilleryModel.Text, textBoxAltilerTitle.Text, Convert.ToInt16(numericUpDownaltiler.Value));
 
                 await Task.Run(() => writedatatoexcel = new WriteDataToExcel(datamodel));
 
@@ -37,26 +37,19 @@ namespace WindowsFormsApplication1
                 MessageBox.Show("Խնդրում ենք լրացնել բոլոր դաշտերը");
         }
 
-        private void buttonSaveDB_Click(object sender, EventArgs e)
+        private async void buttonSaveDB_Click(object sender, EventArgs e)
         {
-            InsertData();
-        }
-        public void InsertData()
-        {
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            WriteDataToDB writeDataToDB;
+            int chechTextbox=ChechTextBoxText();
+            if (chechTextbox != -1)
             {
-                using (SqlCommand sqlCommand = new SqlCommand())
-                {
-                    sqlCommand.Connection = sqlConnection;
-                    sqlConnection.Open();
-                    sqlCommand.CommandText = @"INSERT INTO SolderTable(PassportID,Soldername,Soldersurname,Soldermiddlename,Solderage) VALUES ('" + textBoxPassportID.Text + "','" + textboxFirstName.Text + "','" + textBoxLastName.Text + "','" + textBoxFName.Text + "','" + numericUpDownAge.Value + "')";
-                    sqlCommand.CommandText = "INSERT INTO solderTitle(passportID,solderTitle,solderClassical,solderCompany,solderBattalion,solderBowl) VALUES ('" + textBoxPassportID.Text + "','" + comboBoxTitle.SelectedText + "','" + comboBoxClassical.SelectedText + "','" + comboBoxCompany.SelectedText + "','" + comboBoxBattalion.SelectedText + "','" + comboBoxBowl.SelectedText + "')";
-                    sqlCommand.CommandText = "INSERT INTO altilleryTable(passportID,Altilleryname,Altillerymodel,Altillerytitle,Altilleryage) VALUES ('" + textBoxPassportID.Text + "','" + textBoxArtilleryName.Text + "','" + textBoxArtilleryModel.Text + "','" + textBoxAltilerTitle.Text + "','" + numericUpDownaltiler.Value + "')";
-                    sqlCommand.ExecuteNonQuery();
-                }
+                datamodel = new DataModel(textBoxPassportID.Text, textboxFirstName.Text, textBoxLastName.Text, textBoxFName.Text, Convert.ToInt16(numericUpDownAge.Value), comboBoxTitle.Text, comboBoxClassical.Text, comboBoxCompany.Text, comboBoxBattalion.Text, comboBoxBowl.Text, textBoxArtilleryName.Text, textBoxArtilleryModel.Text, textBoxAltilerTitle.Text, Convert.ToInt16(numericUpDownaltiler.Value));
+                await Task.Run(() => writeDataToDB = new WriteDataToDB(datamodel));
             }
-            MessageBox.Show("Բազան հաջողությամբ լրացվեց");
+            else
+                MessageBox.Show("Խնդրում ենք լրացնել բոլոր դաշտերը");
         }
+        
         private int ChechTextBoxText()
         {
             if (textBoxAltilerTitle.Text != string.Empty && textBoxArtilleryModel.Text != string.Empty && textBoxArtilleryName.Text != string.Empty && textboxFirstName.Text != string.Empty && textBoxFName.Text != string.Empty && textBoxLastName.Text != string.Empty && textBoxPassportID.Text != string.Empty)
---
+++ WindowsFormsApplication1/WriteDataToDB.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    class WriteDataToDB
+    {
+        private string connectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\User\Source\Repos\Console\Military-Program.-Artillery-Brigades3\WindowsFormsApplication1\bin\Debug\armydata.mdf;Integrated Security=True";
+
+        public WriteDataToDB()
+        {
+
+        }
+
+        public WriteDataToDB(DataModel datamodel)
+        {
+            InsertData(datamodel);
+        }
+        public void InsertData(DataModel datamodel)
+        {
+            
+                using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand sqlCommand = new SqlCommand())
+                    {
+                        sqlCommand.Connection = sqlConnection;
+                        sqlConnection.Open();
+                        sqlCommand.CommandText = "INSERT INTO SolderTable(PassportID,Soldername,Soldersurname,Soldermiddlename,Solderage) VALUES ('" + datamodel.solderPassportID + "',N'" + datamodel.SolderName + "',N'" + datamodel.SoldeSurername + "',N'" + datamodel.Solderfname + "','" + datamodel.Solderage + "')";
+                        sqlCommand.ExecuteNonQuery();
+                        sqlCommand.CommandText = "INSERT INTO solderTitle(passportID,solderTitle,solderClassical,solderCompany,solderBattalion,solderBowl) VALUES ('" + datamodel.solderPassportID + "',N'" + datamodel.Soldertitle + "','" + datamodel.Solderclassical + "','" + datamodel.Soldercompany + "','" + datamodel.Solderbattalion + "','" + datamodel.Solderbowl + "')";
+                        sqlCommand.ExecuteNonQuery();
+                        sqlCommand.CommandText = "INSERT INTO altilleryTable(passportID,Altilleryname,Altillerymodel,Altillerytitle,Altilleryage) VALUES ('" + datamodel.solderPassportID + "',N'" + datamodel.Artilleryname + "',N'" + datamodel.Artillerymodel + "',N'" + datamodel.Altilertitle + "','" + datamodel.Altilerage + "')";
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                }
+                MessageBox.Show("Բազան հաջողությամբ լրացվեց");
+            
+            
+        }
+    }
+}
